258585ac9b8e644f16401eb0c6f2d397adeacb34
MyPC <MyPC@OFALSA>
2022-03-30 22:51:53 +0800

    Final Commit

--- src/BinaryProgram.java
+++ src/BinaryProgram.java
@@ -4,7 +4,7 @@ public class BinaryProgram {
 	public static void main(String[] args) {
 		/******************************************************************
 		 * Program 1: Conversion from binary to decimal representation
-		 * Programmer:
+		 * Programmers:
 		 * 				Jerick Navales
 						Angelyn E. Calonia
 						JudyRose  D. Godilo
@@ -40,36 +40,7 @@ public class BinaryProgram {
 		         int bit;
 		      
 		      	
-		       	//////////////////////////////////////
-		      	// ADD YOUR CODE HERE
-		      	// FOLLOW THE EXAMPLE PROVIDED
-		      	// FOR THE LAST BIT, THE 6th
-		      	//////////////////////////////////////
-		/*      				
-		         bit=binary%10;//get the last bit (6th)
-		         decimal=decimal+bit*1;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 6th bit, now the 5th bit is last
-		      	
-		         bit=binary%10;//get the last bit (5th)
-		         decimal=decimal+bit*2;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 5th bit, now the 4th bit is last
-		      	
-		         bit=binary%10;//get the last bit (4th)
-		         decimal=decimal+bit*4;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 4th bit, now the 3rd bit is last
-		      	
-		         bit=binary%10;//get the last bit (3rd)
-		         decimal=decimal+bit*8;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 3rd bit, now the 2nd bit is last
-		      	
-		         bit=binary%10;//get the last bit (2nd)
-		         decimal=decimal+bit*16;//add it, multiplied by the corresponding power of 2
-		         binary=binary/10;//get rid of the 2nd bit, now the 1st bit is last 
-		      	
-		         bit=binary%10;//get the last bit (1st)
-		         decimal=decimal+bit*32;//add it, multiplied by the corresponding power of 2
-		*/
-
+		       	
 			int i=0;
 			int power2=1;
 			int len=binaryString.length();
